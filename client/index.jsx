aefc86c9915acfbdec4daf08518e7761c189b65e
NitroFray <32951247+NitroFray@users.noreply.github.com>
2022-02-14 11:55:53 +0100

    Setup initial project

---
+++ client/index.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>HELLOOOOO!!</h1>, document.getElementById("app"));
---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/test", (req, res) => {
+  res.send("hello from server");
+});
+
+app.use(express.static(path.resolve("../client/dist")));
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${server.address().port}`);
+});
